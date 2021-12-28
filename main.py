2ec1e29bddf3093ed6434d495bdb23beca70df83
meric-cpm <meric.chenu@efrei.net>
2021-12-28 15:41:54 +0100

    Ajout de l'interface

--- main.py
+++ main.py
@@ -1,36 +1,53 @@
 from read_files import *
 from algo_floyd_warshall import *
-print("                                         -------------------------------------------------------------------------------")
-print("                                         |  Bienvenue dans notre application dediée à l'algorithme de Floyd Warshall !  |")
-print("                                         -------------------------------------------------------------------------------")
+print("                                 ----------------------------------------------------------------------------------------------")
+print("                                 | Bienvenue dans notre application dediée aux algorithmes de plus court chemin d'un graphe ! |")
+print("                                 ----------------------------------------------------------------------------------------------")
 print("\n               Au sein de cette application, vous aurez la possibilité de renseigner un graphique sous forme de format text")
 print("               Nous vous renverrons dans un premier temps l'affichage de votre graphique")
-print("               Ensuite, nous nous chargerons d'affecter l'algorithme de Floyd Warshall à votre graphe afin \n               de vous renvoyer les plus court chemins associés à vos sommets\n\n")
+print("               Ensuite, nous nous chargerons d'affecter l'algorithme de votre choix à votre graphe afin \n               de vous renvoyer les plus court chemins associés à vos sommets\n\n")
 keep_going = True
 while(keep_going):
-    print("         -----------------------------------------------------------------------------------------------------------------------------------------")
-    print("         | Tapez 1 pour afficher votre graphe et les plus court chemins    | Tapez 2 pour afficher votre graphe | Tapez autre chose pour quitter |")
-    print("         -----------------------------------------------------------------------------------------------------------------------------------------\n")
+    print("                                     -------------------------------------------------------------------")
+    print("                                     |       Tapez 1 pour écrire un graphe en fichier texte            |")
+    print("                                     |       Tapez 2 pour choisir l'algorithme de Floyd Warshall       |")
+    print("                                     |       Tapez 3 pour choisir l'algorithme de Dijkstra             |")
+    print("                                     |       Tapez 4 pour choisir l'algorithme de Bellman              |")
+    print("                                     |       Tapez autre chose pour quitter                            |")
+    print("                                     -------------------------------------------------------------------")
     input_user = input("Faites votre choix : ")
-
-    if(input_user != '1' and input_user != '2'):
-        keep_going = False
+    if(input_user == '1'):
+        write_graph()
     elif(input_user == '2'):
-        file_name = input("Entrez le nom de votre fichier, sans l'extension : ")
-        try:
-            Graph = read_graph(file_name)
-            display_array_graph(Graph.array_transitions)
-        except:
-            print("fichier introuvable !")
+        print("         -----------------------------------------------------------------------------------------------------------------------------------------")
+        print("         | Tapez 1 pour afficher votre graphe et les plus court chemins    | Tapez 2 pour afficher votre graphe | Tapez autre chose pour quitter |")
+        print("         -----------------------------------------------------------------------------------------------------------------------------------------\n")
+        input_user = input("Faites votre choix : ")
+
+        if(input_user != '1' and input_user != '2'):
+            keep_going = False
+        elif(input_user == '2'):
+            file_name = input("Entrez le nom de votre fichier, sans l'extension : ")
+            try:
+                Graph = read_graph(file_name)
+                display_array_graph(Graph.array_transitions)
+            except:
+                print("fichier introuvable !")
+        else:
+            file_name = input("Entrez le nom de votre fichier, sans l'extension : ")
+            try:
+                Graph = read_graph(file_name)
+                display_array_graph(Graph.array_transitions)
+                array_distance, array_path = floyd_warshall(Graph)
+                tuple_result = display_solution(array_path, array_distance)
+                array_shortest_path = tuple_result[0]
+                array_display_graph = tuple_result[1]
+                write_graph_Floyd_Warshall(file_name,array_shortest_path,array_display_graph,Graph)
+            except:
+                print("fichier introuvable !")
+    elif(input_user=='3'):
+        print("Vous allez utiliser l'algorithme de Dikstra")
+    elif(input_user == '4'):
+        print("Vous allez utiliser l'algorithme de Bellman")
     else:
-        file_name = input("Entrez le nom de votre fichier, sans l'extension : ")
-        try:
-            Graph = read_graph(file_name)
-            display_array_graph(Graph.array_transitions)
-            array_distance, array_path = floyd_warshall(Graph)
-            tuple_result = display_solution(array_path, array_distance)
-            array_shortest_path = tuple_result[0]
-            array_display_graph = tuple_result[1]
-            write_graph(file_name,array_shortest_path,array_display_graph,Graph)
-        except:
-            print("fichier introuvable !")
+        keep_going = False
--- read_files.py
+++ read_files.py
@@ -1,5 +1,5 @@
 from class_graph import *
-
+import re
 #Fonction nous permettant de lire un graphe sous forme de fichier texte
 def read_graph(file_name):
     RepresentationGraph = Graph(0,0,[])
@@ -39,7 +39,107 @@ def read_graph(file_name):
     RepresentationGraph.array_transitions = new_array_graph
     return RepresentationGraph
 
-def write_graph(file_name,shortest_path,array_display_graph,Graph_information):
+def write_graph():
+    keep_going = True
+    while(keep_going):
+        print("\nAfin d'entrer le nom de votre fichier, vous devez respecter la syntaxe suivante : G1-Graphe[numéro de votre fichier]")
+        print("Exemple : G1-Graphe2\n")
+        file_name = input("Entrez le nom du fichier, sans l'extension, dans lequel vous voulez écrire votre graphe : ")
+        pattern = "^G1-Graphe[1-9]$"
+        result = re.match(pattern,file_name)
+        if(result):
+            entry_file = []
+            input_number_vertices = input("Entrez le nombre de sommets de votre graphe : ")
+            array_number = ["0","1","2","3","4","5","6","7","8","9"]
+            index = 0
+            verification_input = True
+            #Suppression des espaces de l'entrée utilisateur
+            input_number_vertices = re.sub(" ","",input_number_vertices)
+            while(index < len(input_number_vertices)):
+                #Si l'utilisateur a entré un caractère autre qu'un chiffre
+                if(input_number_vertices[index] not in input_number_vertices):
+                    print("Vous n'avez pas entré un nombre !\nImpossible d'écrire cela dans le fichier")
+                    verification_input = False
+                    index = len(input_number_vertices)
+                index+=1
+            if(verification_input == False):
+                input_user = input("Tapez 1 pour continuer, ou quelque chose d'autre sinon : ")
+                input_user = re.sub(" ","",input_user)
+                if(input_user != '1'):
+                    keep_going = False
+            input_number_vertices += "\n"
+            entry_file.append(input_number_vertices)
+            if(keep_going and verification_input):
+                input_number_arcs = input("Entrez le nombre d'arcs de votre graphe : ")
+                input_number_arcs = re.sub(" ", "", input_number_arcs)
+                index = 0
+                while (index < int(input_number_arcs)):
+                    # Si l'utilisateur a entré un caractère autre qu'un chiffre
+                    if (input_number_arcs[index] not in input_number_arcs):
+                        print("Vous n'avez pas entré un nombre !\nImpossible d'écrire cela dans le fichier")
+                        verification_input = False
+                        index = len(input_number_arcs)
+                    index += 1
+                if (verification_input == False):
+                    input_user = input("Tapez 1 pour continuer, ou quelque chose d'autre sinon : ")
+                    input_user = re.sub(" ", "", input_user)
+                    if (input_user != '1'):
+                        keep_going = False
+            input_number_arcs += "\n"
+            entry_file.append(input_number_arcs)
+            #liste comprenant la numérotation des sommets du graphe
+            list_vertices = []
+            for vertices in range(len(input_number_vertices)):
+                list_vertices.append(str(vertices))
+            if(keep_going and verification_input):
+                number_vertices = 0
+                pattern_vertice = "^[0-9]$"
+                while int(number_vertices) < int(input_number_vertices):
+                    print("Arc numéro ",number_vertices+1)
+                    initial_state = input("Entrez le sommet initial de cet arc : ")
+                    initial_state = re.sub(" ", "", initial_state)
+                    result = re.match(pattern_vertice, initial_state)
+                    while(not result or initial_state not in list_vertices):
+                        print("Votre sommet doit être un nombre entier compris dans la liste : ",list_vertices)
+                        initial_state = input("Entrez le sommet initial de cet arc : ")
+                        initial_state = re.sub(" ", "", initial_state)
+                        result = re.match(pattern_vertice, initial_state)
+                    number_vertices+=1
+                    final_state = input("Entrez le sommet final de cet arc : ")
+                    final_state = re.sub(" ", "", final_state)
+                    result = re.match(pattern_vertice, final_state)
+                    while(not result or final_state not in list_vertices):
+                        print("Votre sommet doit être un nombre entier compris dans la liste : ",list_vertices)
+                        final_state = input("Entrez le sommet initial de cet arc : ")
+                        final_state = re.sub(" ", "", final_state)
+                        result = re.match(pattern_vertice, final_state)
+                    weight = input("Entrez le poids de votre arc : ")
+                    weight = re.sub(" ","",weight)
+                    pattern_weight = "^?-[0-9]$"
+                    result = re.match(pattern_weight,weight)
+                    while(not result):
+                        print("Votre poids doit être un nombre.")
+                        weight = input("Entrez le sommet initial de cet arc : ")
+                        print("weight : ",weight)
+                        weight = re.sub(" ", "", weight)
+                        result = re.match(pattern_weight, weight)
+                    arc = initial_state
+                    arc+= " "
+                    arc+= final_state
+                    arc+= " "
+                    arc += weight
+                    arc+= "\n"
+                    entry_file.append(arc)
+            print(entry_file)
+
+        else:
+            print("\nVotre nom de fichier ne respecte pas la syntaxe indiqué.")
+            input_user = input("Tapez 1 pour écrire dans un autre fichier, ou quelque chose d'autre pour quitter : ")
+            input_user = re.sub(" ", "", input_user)
+            if (input_user != '1'):
+                keep_going = False
+
+def write_graph_Floyd_Warshall(file_name,shortest_path,array_display_graph,Graph_information):
 
     Graph_information.number_vertices = " ".join(str (elem) for elem in Graph_information.number_vertices)
     Graph_information.number_arcs = " ".join(str(elem) for elem in Graph_information.number_arcs)
